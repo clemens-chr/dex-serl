716f934feec3bc1a5dbc593c32e76cf31a32291a
clemens-chr <cchristoph@student.ethz.ch>
2025-04-10 04:42:25 +0000

    new exp added

--- examples/experiments/mappings.py
+++ examples/experiments/mappings.py
@@ -3,10 +3,12 @@ from experiments.usb_pickup_insertion.config import TrainConfig as USBPickupInse
 from experiments.object_handover.config import TrainConfig as ObjectHandoverTrainConfig
 from experiments.egg_flip.config import TrainConfig as EggFlipTrainConfig
 from experiments.pick_cube_sim.config import TrainConfig as PickCubeSimTrainConfig
+from experiments.orca_pick_cube_sim.config import TrainConfig as ORCAPickCubeSimTrainConfig
 CONFIG_MAPPING = {
                 "ram_insertion": RAMInsertionTrainConfig,
                 "usb_pickup_insertion": USBPickupInsertionTrainConfig,
                 "object_handover": ObjectHandoverTrainConfig,
                 "egg_flip": EggFlipTrainConfig,
                 "pick_cube_sim": PickCubeSimTrainConfig,
+                "orca_pick_cube_sim": ORCAPickCubeSimTrainConfig,
                }
